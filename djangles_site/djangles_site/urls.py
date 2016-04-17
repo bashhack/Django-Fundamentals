f21a75486c0981ef13a264a9530fb905c947b0dc
bashhack <info@marclaughton.com>
2016-04-17 13:48:44 -0500

    Finishing markup and Django code
    Finishing markup and Django code

--- djangles_site/djangles_site/urls.py
+++ djangles_site/djangles_site/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
 urlpatterns = [
-    url(r'^courses/', include('courses.urls')),
+    url(r'^courses/', include('courses.urls', namespace='courses')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.hello_world),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
